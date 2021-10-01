8c4155e9197c945aece5cbc49cf44ea4b0eae3c9
Germano Carvalho <germano@unimednatal.com.br>
2021-10-01 17:23:28 -0300

    places microservice

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+
+Amplify.configure();
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
